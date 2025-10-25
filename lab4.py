d25d7ea9d192282a049d075a3e1bf7a4c109b19e
nilsukeskin <nilsu.keskin@std.yeditepe.edu.tr>
2025-10-26 00:57:28 +0300

    add today's lab

--- lab4.py
+++ lab4.py
@@ -13,3 +13,4 @@ def throw_rock (m, v0, theta):
           "Kinetic energy at the maximum height is %8.2e J" % (m, v0, theta*180/np.pi, tf, R, hm, vh, Kh))
     return tf, R, hm, vh, Kh
 
+myresult= throw_rock(1.5, 0.3, 35.20)
